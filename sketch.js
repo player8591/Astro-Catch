b6f7b1c3ea8d85dbd4f894783b26dd31067788f5
player8591 <lirkastrati79@gmail.com>
2025-03-09 10:37:38 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,3 +5,11 @@ function setup() {
 function draw() {
   background(220);
 }
+function setup() {
+  createCanvas(400, 400);
+  console.log("Sketch.js is running!"); // Debugging message
+}
+
+function draw() {
+  background(220);
+}
